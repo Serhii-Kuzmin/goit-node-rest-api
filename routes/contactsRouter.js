ed62b2b4fa9fe36237ae7727a25b2fa05fea1a17
Serhii-Kuzmin <seryy8248@gmail.com>
2024-02-19 17:59:17 +0100

    homework-03

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,16 +1,37 @@
 import express from "express";
 import contactsControllers from "../controllers/contactsControllers.js";
+import validateBody from "../decorators/validateBody.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import isValidId from "../middlewares/isValidId.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getAllContacts);
 
-contactsRouter.get("/:id", contactsControllers.getOneContact);
+contactsRouter.get("/:id", isValidId, contactsControllers.getOneContact);
 
-contactsRouter.delete("/:id", contactsControllers.deleteContact);
+contactsRouter.delete("/:id", isValidId, contactsControllers.deleteContact);
 
-contactsRouter.post("/", contactsControllers.createContact);
+contactsRouter.post(
+  "/",
+  validateBody(createContactSchema),
+  contactsControllers.createContact
+);
 
-contactsRouter.put("/:id", contactsControllers.updateContact);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(updateContactSchema),
+  contactsControllers.updateContact
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  contactsControllers.updateStatusContact
+);
 
 export default contactsRouter;
